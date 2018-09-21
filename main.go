7ef9175ce4e6ac31130eac182cba6ccd1d59daeb
Daniel Bornkessel <tcx-merge@bornkessel.com>
2018-09-21 10:01:22 +0200

    First working, rough version

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
 
 type TPX struct {
@@ -14,16 +15,18 @@ type TPX struct {
 }
 
 type Trackpoint struct {
-	Time     string
+	Time     time.Time
 	Position struct {
 		LatitudeDegrees  string `xml:",omitempty"`
 		LongitudeDegrees string `xml:",omitempty"`
 	} `xml:",omitempty"`
 	AltitudeMeters string `xml:",omitempty"`
 	DistanceMeters string `xml:",omitempty"`
-	HeartRateBpm   struct {
-		Value string `xml:",omitempty"`
+
+	HeartRateBpm struct {
+		Value int `xml:",omitempty"`
 	} `xml:",omitempty"`
+
 	Extensions struct {
 		TPX TPX
 	} `xml:",omitempty"`
@@ -55,17 +58,20 @@ type TCX struct {
 			Sport string `xml:"Sport,attr"`
 			Id    string
 			Lap   []struct {
-				StartTime           string `xml:"StartTime,attr"`
-				TotalTimeSeconds    string `xml:",omitempty"`
-				DistanceMeters      string `xml:",omitempty"`
-				MaximumSpeed        string `xml:",omitempty"`
-				Calories            string `xml:",omitempty"`
+				StartTime        time.Time `xml:"StartTime,attr"`
+				TotalTimeSeconds string    `xml:",omitempty"`
+				DistanceMeters   string    `xml:",omitempty"`
+				MaximumSpeed     string    `xml:",omitempty"`
+				Calories         string    `xml:",omitempty"`
+
 				AverageHeartRateBpm struct {
-					Value string `xml:",omitempty"`
+					Value int `xml:",omitempty"`
 				} `xml:",omitempty"`
+
 				MaximumHeartRateBpm struct {
-					Value string `xml:",omitempty"`
+					Value int `xml:",omitempty"`
 				} `xml:",omitempty"`
+
 				Intensity     string `xml:",omitempty"`
 				TriggerMethod string `xml:",omitempty"`
 				Track         struct {
@@ -76,28 +82,143 @@ type TCX struct {
 	}
 }
 
+func trackpointIterator(tcx TCX) *chan Trackpoint {
+	c := make(chan Trackpoint)
+
+	go func() {
+		for _, lap := range tcx.Activities.Activity.Lap {
+			for _, trackpoint := range lap.Track.Trackpoint {
+				c <- trackpoint
+			}
+		}
+		close(c)
+	}()
+
+	return &c
+}
+
+func replaceTrackpoints(tcx TCX, trackpoints []Trackpoint) TCX {
+	lapCnt := 0
+	latestTrackpoint := trackpoints[len(trackpoints)-1].Time
+	laps := tcx.Activities.Activity.Lap
+
+	getNextLapStartTime := func() time.Time {
+		if lapCnt == len(laps)-1 {
+			return latestTrackpoint.Add(time.Second)
+		}
+
+		return laps[lapCnt+1].StartTime
+	}
+
+	nextLapStartTime := getNextLapStartTime()
+	newTrackpoints := []Trackpoint{}
+
+	type BpmInfo struct {
+		cnt   int
+		max   int
+		total int
+	}
+
+	bpmInfo := BpmInfo{}
+
+	for _, trackpoint := range trackpoints {
+		if trackpoint.Time.After(nextLapStartTime) || trackpoint.Time.Equal(nextLapStartTime) {
+			tcx.Activities.Activity.Lap[lapCnt].Track.Trackpoint = newTrackpoints
+			if bpmInfo.cnt > 0 {
+				tcx.Activities.Activity.Lap[lapCnt].AverageHeartRateBpm.Value = bpmInfo.total / bpmInfo.cnt
+				tcx.Activities.Activity.Lap[lapCnt].MaximumHeartRateBpm.Value = bpmInfo.max
+			}
+
+			bpmInfo = BpmInfo{}
+			newTrackpoints = []Trackpoint{}
+			lapCnt++
+			nextLapStartTime = getNextLapStartTime()
+			continue
+		}
+
+		newTrackpoints = append(newTrackpoints, trackpoint)
+		if bpm := trackpoint.HeartRateBpm.Value; bpm != 0 {
+			bpmInfo.cnt++
+			if bpmInfo.max < bpm {
+				bpmInfo.max = bpm
+			}
+			bpmInfo.total += bpm
+		}
+	}
+	tcx.Activities.Activity.Lap[lapCnt].Track.Trackpoint = newTrackpoints
+
+	return tcx
+}
+
+func mergeTcx(master TCX, bpm TCX) (TCX, error) {
+	trackpoints := []Trackpoint{}
+
+	masterIter := trackpointIterator(master)
+	bpmIter := trackpointIterator(bpm)
+
+	masterTp, moreMasterTp := <-*masterIter
+	bpmTp, moreBpmTp := <-*bpmIter
+
+loop:
+	for {
+		switch {
+		case !moreMasterTp && !moreBpmTp:
+			break loop
+
+		case (moreMasterTp && !moreBpmTp) || (moreMasterTp && masterTp.Time.Before(bpmTp.Time)):
+			trackpoints = append(trackpoints, masterTp)
+			masterTp, moreMasterTp = <-*masterIter
+
+		case moreBpmTp && !moreMasterTp || (moreMasterTp && masterTp.Time.After(bpmTp.Time)):
+			// we only want the heart beat info and the time stamp
+			trackpoints = append(trackpoints, Trackpoint{HeartRateBpm: bpmTp.HeartRateBpm, Time: bpmTp.Time, Position: trackpoints[len(trackpoints)-1].Position})
+			bpmTp, moreBpmTp = <-*bpmIter
+
+		case masterTp.Time == bpmTp.Time:
+			masterTp.HeartRateBpm = bpmTp.HeartRateBpm
+			trackpoints = append(trackpoints, masterTp)
+			masterTp, moreMasterTp = <-*masterIter
+			bpmTp, moreBpmTp = <-*bpmIter
+		}
+	}
+
+	return replaceTrackpoints(master, trackpoints), nil
+}
+
 func main() {
 	masterFileName := flag.String("master-tcx", "", "file name of the tcx that should be enriched with heartbeat data")
+	bpmFileName := flag.String("bpm-tcx", "", "file name of the tcx that contains bpm information")
 	flag.Parse()
 
-	if *masterFileName == "" {
+	if *masterFileName == "" || *bpmFileName == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	masterContent, err := ioutil.ReadFile(*masterFileName)
-
 	if err != nil {
 		log.Fatalf("error reading file %s: %s\n", *masterFileName, err)
 	}
 
-	var tcx TCX
-	err = xml.Unmarshal([]byte(masterContent), &tcx)
+	bpmContent, err := ioutil.ReadFile(*bpmFileName)
+	if err != nil {
+		log.Fatalf("error reading file %s: %s\n", *bpmFileName, err)
+	}
 
-	output, err := xml.MarshalIndent(tcx, "", "  ")
+	var masterTcx TCX
+	err = xml.Unmarshal([]byte(masterContent), &masterTcx)
+
+	var bpmTcx TCX
+	err = xml.Unmarshal([]byte(bpmContent), &bpmTcx)
+
+	mergedTcx, err := mergeTcx(masterTcx, bpmTcx)
+	if err != nil {
+		log.Fatalf("error merging tcx files: %s", err)
+	}
+
+	output, err := xml.MarshalIndent(mergedTcx, "", "  ")
 	if err != nil {
 		log.Fatalf("error: %v\n", err)
 	}
-
 	os.Stdout.Write(output)
 }
